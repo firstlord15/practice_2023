9344398f96aeed5818a8aadec667b455b57dc69a
Ratmir <ratmir.yuldashev28@mail.ru>
2023-06-22 00:46:40 +0300

    fix insert database (i'm just forger about log_id)

--- src/tests/test.py
+++ src/tests/test.py
@@ -5,18 +5,18 @@ import psycopg2
 
 start_time = time.time()
 
-access_log_pattern = re.compile(r'^(\S+) \S+ \S+ \[(.*?)] "(GET|POST) (.*?) HTTP/1\.[01]" (\d+) (\d+) "(.*?)" "(.*?)"$')
-access_success_pattern = re.compile(r'^(\S+) \S+ \S+ \[(.*?)] "(GET|POST) (.*?) HTTP/1\.[01]" 200 (\d+)$')
-access_failure_pattern = re.compile(r'^(\S+) \S+ \S+ \[(.*?)] "(GET|POST) (.*?) HTTP/1\.[01]" 4\d{2} (\d+)$')
-tortoise_svn_pattern = re.compile(r'^(\S+) \S+ \S+ \[(.*?)] "(PROPFIND|CHECKOUT) (.*?) HTTP/1\.[01]" 401 (\d+)$')
+access_log_pattern = r'^(\S+) \S+ \S+ \[(.*?)\] "(GET|POST) (.*?) HTTP/1\.[01]" (\d+) (\d+) "(.*?)" "(.*?)"$'
+access_success_pattern = r'^(\S+) \S+ \S+ \[(.*?)\] "(GET|POST) (.*?) HTTP/1\.[01]" 200 (\d+)$'
+access_failure_pattern = r'^(\S+) \S+ \S+ \[(.*?)\] "(GET|POST) (.*?) HTTP/1\.[01]" 4\d{2} (\d+)$'
+tortoise_svn_pattern = r'^(\S+) \S+ \S+ \[(.*?)\] "(PROPFIND|CHECKOUT) (.*?) HTTP/1\.[01]" 401 (\d+)$'
 
 
-def access_log(title, ip, timestamp, http, url, status_code, response_size):
+def access_log(title, ip, timestamp, method, url, status_code, response_size):
     output = {
         'Title': title,
         'IP': ip,
         'Timestamp': timestamp,
-        'Method': http,
+        'Method': method,
         'URL': url,
         'Status': status_code,
         'Response': response_size
@@ -32,11 +32,12 @@ try:
 
     # Подготовка подготовленного запроса для вставки данных
     insert_query = """
-        INSERT INTO Access_logs (title_status_log, ip_address, request_time, request_path, status_code, response_size)
-        SELECT %s, %s, %s, %s, %s, %s
+        INSERT INTO Access_logs (log_id, title_status_log, ip_address, request_time, request_path, status_code, response_size)
+        SELECT %s, %s, %s, %s, %s, %s, %s
         WHERE NOT EXISTS (
             SELECT 1 FROM Access_logs
-            WHERE title_status_log = %s
+            WHERE log_id = %s
+                AND title_status_log = %s
                 AND ip_address = %s
                 AND request_time = %s
                 AND request_path = %s
@@ -52,32 +53,36 @@ try:
 
     access_logs = []
     for log in logs:
-        if match := access_success_pattern.match(log):
-            access_logs.append(access_log("Apache access log (success - code 200):", match.group(1), match.group(2),
-                                          match.group(3), match.group(4), '200',
-                                          match.group(5)))
 
-        elif match := access_log_pattern.match(log):
+        if match := re.match(access_success_pattern, log):
+            access_logs.append(access_log("Apache access log (success - code 200):", match.group(1), match.group(2),
+                                          match.group(3), match.group(4), '200', match.group(5)))
+
+        elif match := re.match(access_log_pattern, log):
             access_logs.append(access_log("Apache access log:", match.group(1), match.group(2),
                                           match.group(3), match.group(4), match.group(5),
                                           match.group(6)))
 
-        elif match := access_failure_pattern.match(log):
+        elif match := re.match(access_failure_pattern, log):
             access_logs.append(access_log("Apache access log (failure - code 4xx):", match.group(1),
                                           match.group(2), match.group(3), match.group(4), log.split()[-2],
                                           match.group(5)))
 
-        elif match := tortoise_svn_pattern.match(log):
+        elif match := re.match(tortoise_svn_pattern, log):
             access_logs.append(
                 access_log("Apache unnacepted request methods (caused by TortoiseSVN):", match.group(1), match.group(2),
                            match.group(3), match.group(4),
                            log.split()[-2], match.group(5)))
 
     # Выполнение подготовленного запроса для каждой записи
+    # for item in access_logs:
+    #     print(item)
     insert_data = [
-        (item['Title'], item['IP'], datetime.strptime(item['Timestamp'], "%d/%b/%Y:%H:%M:%S %z"),
+        (log_id, item['Title'], item['IP'], datetime.strptime(item['Timestamp'], "%d/%b/%Y:%H:%M:%S %z"),
+         item['URL'], int(item['Status']), int(item['Response']), log_id,
+         item['Title'], item['IP'], datetime.strptime(item['Timestamp'], "%d/%b/%Y:%H:%M:%S %z"),
          item['URL'], int(item['Status']), int(item['Response']))
-        for item in access_logs
+        for log_id, item in enumerate(access_logs, start=1)
     ]
 
     cursor.executemany(insert_query, insert_data)
