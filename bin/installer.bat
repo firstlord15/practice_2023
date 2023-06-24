995c7c4634ab748d6882db62bdfabe5c6982cc33
Ratmir <ratmir.yuldashev28@mail.ru>
2023-06-24 18:52:44 +0300

    all ready, now I need fix API

---
+++ bin/installer.bat
@@ -0,0 +1,9 @@
+@echo off
+rem устанавливаем зависимости из requirements.txt
+pip install -r ../src/requirements.txt
+
+rem задержка закрытия окна командной строки
+pause
+
+rem офф командной строки
+exit
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 import json
 import psycopg2
 
@@ -6,8 +7,22 @@ CONFIG_FILE_PATH = 'config/config.json'
 
 # Загрузка конфигурационного файла
 def load_config():
-    with open(CONFIG_FILE_PATH) as config_file:
-        return json.load(config_file)
+    try:
+        with open(CONFIG_FILE_PATH) as config_file:
+            config = json.load(config_file)
+            required_keys = ["name_programm", "postgres", "database", "log_file_path", "result_files_path",
+                             "user_editable_fields", "log_file_mask"]
+            for key in required_keys:
+                if key not in config:
+                    print(f"Ошибка: отсутствует обязательный ключ '{key}' в файле конфигурации '{CONFIG_FILE_PATH}'.")
+                    exit(1)
+            return config
+    except FileNotFoundError:
+        print(f"Ошибка: файл конфигурации '{CONFIG_FILE_PATH}' не найден.")
+        exit(1)
+    except json.JSONDecodeError:
+        print(f"Ошибка: некорректный формат файла конфигурации '{CONFIG_FILE_PATH}'.")
+        exit(1)
 
 
 # Загатавливаю данны
@@ -39,7 +54,6 @@ def save_config(config):
         json.dump(config, config_file, indent=4)
 
 
-# Редактирование данных в конфигурационном файле
 def edit_config():
     print("\nДоступные разделы для редактирования:")
     for section in config_data["user_editable_fields"]:
@@ -50,9 +64,49 @@ def edit_config():
     if section in config_data["user_editable_fields"]:
         # Редактирование значений в выбранном разделе
         print(f"\nРедактирование раздела '{section}':\n")
-        for key, value in config_data[section].items():
-            new_value = input(f"Введите новое значение для '{key}' (текущее значение: {value}): ")
-            config_data[section][key] = new_value
+        try:
+            while True:
+                for key, value in config_data[section].items():
+                    new_value = input(f"Введите новое значение для '{key}' (текущее значение: {value}, для выхода введите 'exit'): ")
+                    if new_value.lower() == "exit":
+                        break
+                    if key == "log_file_path" and not os.path.exists(new_value):
+                        while not os.path.exists(new_value):
+                            print(f"Файл '{new_value}' не найден.")
+                            new_value = input(f"Введите новое значение для '{key}' (для выхода введите 'exit'): ")
+                            if new_value.lower() == "exit":
+                                break
+                    config_data[section][key] = new_value
+                else:
+                    break
+        except AttributeError:
+            if isinstance(config_data[section], str):
+                new_value = input(f"Введите новое значение для '{section}' (текущее значение: {config_data[section]}, для выхода введите 'exit'): ")
+                if new_value.lower() == "exit":
+                    pass
+                elif section == "log_file_path" and not os.path.exists(new_value):
+                    while not os.path.exists(new_value):
+                        print(f"Файл '{new_value}' не найден.")
+                        new_value = input(f"Введите новое значение для '{section}' (для выхода введите 'exit'): ")
+                        if new_value.lower() == "exit":
+                            break
+                else:
+                    config_data[section] = new_value
+            else:
+                while True:
+                    for value in config_data[section]:
+                        new_value = input(f"Введите новое значение для '{section} -> {value}' (текущее значение: {config_data[section][value]}, для выхода введите 'exit'): ")
+                        if new_value.lower() == "exit":
+                            break
+                        if value == "log_file_path" and not os.path.exists(new_value):
+                            while not os.path.exists(new_value):
+                                print(f"Файл '{new_value}' не найден.")
+                                new_value = input(f"Введите новое значение для '{section} -> {value}' (для выхода введите 'exit'): ")
+                                if new_value.lower() == "exit":
+                                    break
+                        config_data[section][value] = new_value
+                    else:
+                        break
 
         # Проверка подключения к базе данных после изменения данных
         if section == "postgres":
@@ -63,6 +117,8 @@ def edit_config():
         print("Конфигурационный файл успешно обновлен.\n")
     else:
         print("Неверный раздел для редактирования.\n")
+        edit_config()
+
 
 
 # Проверка подключения к базе данных PostgreSQL
@@ -80,6 +136,7 @@ def check_postgres_connection(config):
     except psycopg2.Error as e:
         print("Ошибка подключения к базе данных PostgreSQL:")
         print(e)
+        exit(1)
 
 
 # Главная функция программы
